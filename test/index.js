dfa499cf0e00685a52375083491c8d647e3cfa67
Justin de Guzman <justin@justindeguzman.net>
2015-08-16 21:42:52 -1000

    add tests

--- test/index.js
+++ test/index.js
@@ -1,5 +1,35 @@
 
+/*
+ * Module dependencies.
+ */
+
 var mongoose = require('mongoose');
-var modelLoader = require('./../lib');
-modelLoader(__dirname + '/models');
-mongoose.model('User');
+var loader = require('./../lib');
+var should = require('should');
+
+describe('mongoose-model-loader', function() {
+
+  /*
+   * Setup.
+   */
+
+  before(function() {
+    loader(__dirname + '/models');
+  });
+
+  /*
+   * Tests.
+   */
+
+  it('should load the User model', function() {
+    mongoose.model('User').should.exist;
+  });
+
+  it('should load a model with ObjectId', function() {
+    mongoose.model('Tweet').should.exist;
+  });
+
+  it('should CamelCaps hyphenated model names', function() {
+    mongoose.model('BlogPost').should.exist;
+  });
+});
